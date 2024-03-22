57b3e8c7408e8cae187df0e6da2f072ec89303a5
Maria Shalunova <shalunova00@gmail.com>
2024-03-22 16:10:12 +0300

    Add product page

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-const menuItems = document.querySelectorAll('.menu__item');
-const hamburger = document.querySelector('.menu__burger');
+const menuItems = document.querySelectorAll(".menu__item");
+const hamburger = document.querySelector(".menu__burger");
 
 menuItems.forEach((menuItem) => {
-  menuItem.addEventListener('click', () => {
-    document.getElementById("menu__toggle").checked = false; 
+  menuItem.addEventListener("click", () => {
+    document.getElementById("menu__toggle").checked = false;
   });
 });
